43feecbb494ff02fa8abbb2c9c5c41df6bbedaf7
svvishjs <andreww92@live.com>
2020-10-19 21:04:26 +0100

    MVP complete

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
 	<section id="app">
-		<h1>todo_list</h1>
+		<h1>toDo</h1>
+		<form v-on:submit.prevent="saveNewItem">
+			<input type="text" v-model="newItem" placeholder="add toDo here" />
+			<button v-on:click="addNewItem">Add toDo Item</button>
+		</form>
 		<ul>
-			<li></li>
+			<li v-for="todo in todos">
+				<span>{{ todo }}</span>
+			</li>
 		</ul>
 	</section>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			todos: ['Buy shopping', 'Clean bathroom', "Car's MOT"],
+			newItem: ''
+		};
+	},
+	methods: {
+		addNewItem: function() {
+			this.todos.push(this.newItem);
+			this.newItem = '';
+		}
+	}
+};
 </script>
 
 <style scoped></style>
